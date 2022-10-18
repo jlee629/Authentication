3462345f834da21df083ab9d95a479e0956ee44e
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-10-18 11:00:55 -0400

    form modified

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -7,32 +7,35 @@ app.js, Jungyu Lee, 301236221, Oct 18 2022
 */
 
 
-
-(function(){
-    function Start()
-    {
-        console.log("App Started...");
+"use strict";
+(function () {
+    function Start() {
+        console.log("App Started!");
+        $("a.delete").on("click", function (event) {
+            if (!confirm("Are you sure?")) {
+                event.preventDefault();
+                location.href = "/contact-list";
+            }
+        });
     }
+    
+    let fname = document.getElementById('first-name');
+    let lname = document.getElementById('last-name');
+    let email = document.getElementById('email');
+    let number = document.getElementById('number');
+    let message = document.getElementById('message');
+    let submit = document.getElementById('submit');
 
-    window.addEventListener("load", Start);    
+    if(submit){
+        submit.addEventListener('click', ()=>{
+            alert("First Name: " + fname.value + '\n' + "Last Name: " + lname.value + '\n' + 
+            "Email: "+email.value +'\n' + "Contact Number: " +number.value + '\n' + 
+            "Message: "+message.value + '\nYour message has sent to me directly. \nI will redirect you to my Home page!');
+            window.location.href = "/home";    
+        });
 
+    }
+    window.addEventListener("load", Start);
 })();
 
-// on Contact page, if submit button is clicked, the information will be shown as a pop up message, and the user will be redirected to home page.
-
-let fname = document.getElementById('first-name');
-let lname = document.getElementById('last-name');
-let email = document.getElementById('email');
-let number = document.getElementById('number');
-let message = document.getElementById('message');
-let submit = document.getElementById('submit');
-
-if(submit){
-    submit.addEventListener('click', ()=>{
-        alert("First Name: " + fname.value + '\n' + "Last Name: " + lname.value + '\n' + 
-        "Email: "+email.value +'\n' + "Contact Number: " +number.value + '\n' + 
-        "Message: "+message.value + '\nYour message has sent to me directly. \nI will redirect you to my Home page!');
-        window.location.href = "/home";    
-    });
-
-}
+    // on Contact page, if submit button is clicked, the information will be shown as a pop up message, and the user will be redirected to home page.
