210c50cad9938f2a124f8f921c889c572106e8bb
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-10-18 00:12:08 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://127.0.0.1/book_store",
+    "URI": "mongodb+srv://won:lfhHZrBy1Ik8guZj@mongodbserver.yfom0su.mongodb.net/book_store?retryWrites=true&w=majority"
+}
